3d7891e56e82e71a688a48722516a8697ed54799
cartervandekamp <cvandekamp@my.bcit.ca>
2022-10-28 15:46:46 -0700

    assignment

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 4grouplive
+## Carter
+
+###### Array
+//Initialise the variable electronics to an array containing the values mouse, keyboard, monitor
+
+###### Function
+// Reorganize and fix this code so it is properly formatted
+
+
+###### Object
+// Create a student object that has the first name value of Mark, last name value as Jacob, age as 31, and the id as A001234.
+The new employee Mark Jacob is 31 years old. The employee ID is A001234.
---
+++ function/function.js
@@ -0,0 +1,18 @@
+// Reorganize and fix this code so it is properly formatted
+// One if statement checks for Integer data type
+// One if statement checks for String data type
+// One else statement that is the default
+// The correct if/else statement will be stored in teh sentence varaible
+// console log out the sentence at the end and add a single line comment.
+
+var z = 2;
+var sentence = ""; {
+if (z == 2) {
+    console.log("The number is the Integer" + z);
+}
+else if (z == "2") {
+    console.log("The number is the String" + z);
+} else {
+    console.log("The number is something else");
+    }
+}
